590d10d7c8039098241458ad0522144be13d81fb
Avel1956 <35372097+Avel1956@users.noreply.github.com>
2022-08-03 15:46:37 -0400

    Update Home.py

--- Home.py
+++ Home.py
@@ -193,7 +193,7 @@ red_metrics_df = pd.DataFrame(data = red_metrics)
 #\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
 # inicialización del objeto pyvis
 autores_net = Network(height='600px',
-                       width='90%',
+                       width='150%',
                        bgcolor='#0c1b33',
                        
                        font_color='white'
