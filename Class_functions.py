bc8c1e0e746fba9236f01eddb56650a692549746
Avel1956 <35372097+Avel1956@users.noreply.github.com>
2022-08-08 09:42:25 -0400

    Update Class_functions.py

--- Class_functions.py
+++ Class_functions.py
@@ -20,50 +20,7 @@ import time
 
 
 
-# def import_drive(range_data, id_sheet):
-# #Funcion para importar una hoja de Google drive como dataframe
-# #Recibe dos parámetros (range, spreadsheet_ID) y devuelve un dataframe 
-# #con los contenidos del archivo
 
-#     #os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "path_to_your_.json_credential_fileF:\Onedrive\Doctorado\Doctorado 2021\software\mlclass\credentials.json"
-#     SCOPES = ['https://www.googleapis.com/auth/spreadsheets.readonly']
-
-
-#     creds = None
-#         # The file token.json stores the user's access and refresh tokens, and is
-#         # created automatically when the authorization flow completes for the first
-#         # time.
-#     if os.path.exists('token.json'):
-#         creds = Credentials.from_authorized_user_file('token.json', SCOPES)
-#     # If there are no (valid) credentials available, let the user log in.
-#     if not creds or not creds.valid:
-#         if creds and creds.expired and creds.refresh_token:
-#             creds.refresh(Request())
-#         else:
-#             flow = InstalledAppFlow.from_client_secrets_file(
-#                 'credentials.json', SCOPES)
-#             creds = flow.run_local_server(port=0)
-#         # Save the credentials for the next run
-#         with open('token.json', 'w') as token:
-#             token.write(creds.to_json())
- 
-
-#     service = build('sheets', 'v4')
-
-#     SAMPLE_SPREADSHEET_ID = id_sheet
-#     SAMPLE_RANGE_NAME = range_data
-#     sheet = service.spreadsheets()
-#     result = sheet.values().get(spreadsheetId=SAMPLE_SPREADSHEET_ID,
-#                                 range=SAMPLE_RANGE_NAME).execute()
-#     values = result.get('values', [])
-#     df = pd.DataFrame.from_records(values, columns=values[0])
-#     df = df.drop(0)
-#     df = df.drop("", axis = 1)# borra columnas son encabezado
-
-#     return(df)
-
-#df = import_drive('Productos!A2:BC300', '1Z0iNrIIMFkt7jtdPLvw4hkObD7-eALUPMNpCWT9-CgI')
-#print (df.head())
 
 
 
